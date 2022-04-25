1c27d348783c0ae90e05ab1ff0cb09f1b7d119d9
gnd-vn <victor.hugo.negosek.de.oliveira@ipt.fraunhofer.de>
2022-04-25 15:32:54 +0200

    Get requests Implemented

--- src/components/MultiStepForm.vue
+++ src/components/MultiStepForm.vue
@@ -1,73 +1,78 @@
 <template>
-  <div class="container">
-    <article>
-      <header>
-        <div class="progress">
+  <div v-for="step in formSteps" :key="step.id">
+    <div v-if="step.id - 1 == activeStep" class="container">
+      <article>
+        <header>
+          <div class="progress">
+            <div
+              v-for="step in formSteps"
+              :key="step.id"
+              class="progress-step"
+              :class="{ active: step.id - 1 == activeStep }"
+            >
+              {{ step.id }}
+            </div>
+          </div>
+        </header>
+        <section :class="animation">
+          <h2>{{ formSteps[activeStep].title }}</h2>
+          <h1>{{ formSteps[activeStep].description }}</h1>
+          <h3>{{ formSteps[activeStep].header }}</h3>
+
           <div
-            class="progress-step"
-            :class="{ active: index === activeStep }"
-            v-for="(step, index) in formSteps"
-            :key="'step' + index"
+            v-for="question in questions"
+            :key="question.id"
+            class="input-fields"
           >
-            {{ index + 1 }}
-          </div>
-        </div>
-      </header>
-      <section :class="animation">
-        <h2>{{ formSteps[activeStep].title }}</h2>
-        <h1>{{ formSteps[activeStep].description }}</h1>
-        <div class="input-fields">
-          <div
-            class="input-container"
-            v-for="(field, index) in formSteps[activeStep].fields"
-            :key="'field' + index"
-          >
-            <h3>
-              {{ field.introduction }}
-            </h3>
-            <label class="input-label">{{ field.label }}</label>
-            <v-slider
-              class="input-slider"
-              v-model="field.value"
-              color="green"
-              :min="0"
-              :max="5"
-              :step="1"
-              :thumb-size="15"
+            <div
+              v-if="question.phase_id - 1 == activeStep"
+              class="input-container"
             >
-              <template v-slot:append>
-                <v-text-field
-                  v-model="field.value"
-                  label="Value"
-                  variant="plain"
-                ></v-text-field>
-              </template>
-            </v-slider>
+              <label class="input-label">{{ question.label }}</label>
+              <v-slider
+                class="input-slider"
+                v-model="question.value"
+                color="green"
+                :min="0"
+                :max="5"
+                :step="1"
+                :thumb-size="15"
+              >
+                <template v-slot:append>
+                  <v-text-field
+                    v-model="question.value"
+                    label="Value"
+                    variant="plain"
+                  ></v-text-field>
+                </template>
+              </v-slider>
+            </div>
           </div>
-        </div>
-        <div>
-          <div class="actions">
-            <v-btn v-if="activeStep + 1 != 1" @click="backStep" color="error">
-              back
-            </v-btn>
-            <v-btn
-              v-if="activeStep < formSteps.length - 1"
-              @click="nextStep"
-              color="sucess"
-            >
-              next
-            </v-btn>
-            <v-btn
-              v-if="activeStep === formSteps.length - 1"
-              @click="nextStep"
-              color="info"
-            >
-              done
-            </v-btn>
+
+          <div>
+            <div class="actions">
+              <v-btn v-if="activeStep + 1 != 1" @click="backStep" color="error">
+                back
+              </v-btn>
+              <v-btn
+                v-if="activeStep < formSteps.length - 1"
+                @click="nextStep"
+                color="sucess"
+              >
+                next
+              </v-btn>
+              <v-btn
+                v-if="activeStep === formSteps.length - 1"
+                @click="nextStep"
+                color="info"
+              >
+                done
+              </v-btn>
+            </div>
           </div>
-        </div>
-      </section>
-    </article>
+        </section>
+      </article>
+    </div>
   </div>
 </template>
 
@@ -77,51 +82,20 @@ export default {
     return {
       activeStep: 0,
       animation: "animate-in",
-      formSteps: [
-        {
-          title: "PERSONAL INFORMATIONS",
-          description: "Please insert your personal informations",
-          fields: [
-            {
-              introduction: "Type the following informations:",
-              label: "Name",
-              value: 1,
-              comment: "optional brieth comment",
-            },
-            {
-              label: "E-Mail",
-              value: 1,
-              comment: "optional brieth comment",
-            },
-          ],
-        },
-        {
-          title: "PLANUNG UND KONZEPTION",
-          description: "Please evaluate the fields betwen 0 and 5",
-          fields: [
-            {
-              label: "NUTZUNG",
-              value: 1,
-              comment: "optional brieth comment",
-            },
-            {
-              label: "QUALITÄT",
-              value: 1,
-              comment: "optional brieth comment",
-            },
-            {
-              label: "MACHINE LEARNING",
-              value: 1,
-              comment: "optional brieth comment",
-            },
-          ],
-        },
-        {
-          title:
-            "Thank you for filling the questions, Click submit for the response!",
-        },
-      ],
+      questions: [],
+      formSteps: [],
     };
+  },
+  mounted() {
+    fetch("http://localhost:5000/formSteps")
+      .then((res) => res.json())
+      .then((data) => (this.formSteps = data))
+      .catch((err) => console.log(err.message));
+
+    fetch("http://localhost:5000/questions")
+      .then((res) => res.json())
+      .then((data) => (this.questions = data))
+      .catch((err) => console.log(err.message));
   },
   methods: {
     nextStep() {
@@ -136,38 +110,34 @@ export default {
 
 <style lang="scss" scoped>
 @import url("https://fonts.googleapis.com/css?family=Noto+Sans&display=swap");
-
 @mixin flexbox() {
   display: flex;
   justify-content: center;
-  align-items: center;
 }
-
 .container {
   @include flexbox();
   width: 100%;
   min-height: 100vh;
   font-family: "Noto Sans", sans-serif;
 }
-
 article {
   display: flex;
   margin: 10px;
   width: calc(100% - 20px);
-  max-width: 1200px;
+  max-width: 1500px;
   min-height: 280px;
-
   header {
     @include flexbox();
+    align-items: center;
     width: 60px;
     height: flex;
     background-color: #fff;
     border-right: 2px inset rgb(25, 163, 94);
     box-shadow: 0 15px 30px rgba(0, 0, 0, 0.2), 0 10px 10px rgba(0, 0, 0, 0.2);
   }
-
   .progress-step {
     @include flexbox();
+    align-items: center;
     position: relative;
     width: 30px;
     height: 30px;
@@ -175,20 +145,16 @@ article {
     margin-bottom: 20px;
     color: #fff;
     background-color: rgb(25, 163, 94);
-
     &.active {
       background-color: rgb(25, 163, 94);
-
       ~ .progress-step {
         color: #555;
         background-color: #ccc;
       }
-
       ~ .progress-step::before {
         background-color: #ccc;
       }
     }
-
     &:before {
       content: "";
       position: absolute;
@@ -198,43 +164,45 @@ article {
       background-color: rgb(25, 163, 94);
       z-index: 10;
     }
-
     &:first-child::before {
       display: none;
     }
   }
-
   section {
     @include flexbox();
     flex-direction: column;
     width: 100%;
     background-color: #fff;
     box-shadow: 0 15px 30px rgba(0, 0, 0, 0.2), 0 10px 10px rgba(0, 0, 0, 0.2);
-
     h2 {
+      text-align: center;
       font-size: 2rem;
       color: rgb(1, 71, 36);
       margin: 0px;
       padding-top: 20px;
     }
     h1 {
+      text-align: center;
       font-size: 0.8rem;
       color: rgb(90, 94, 92);
       margin: 0;
       padding-bottom: 20px;
     }
-
+    h3 {
+      font-size: 1.5rem;
+      color: rgb(1, 71, 36);
+      margin-left: 10px;
+      padding-bottom: 10px;
+    }
     .input-fields {
       @include flexbox();
       flex-direction: column;
       width: 100%;
     }
-
     .input-container {
       position: relative;
       padding: 1px 1pc 1px 1px;
       width: calc(100% - 40px);
-
       input {
         position: relative;
         width: 100%;
@@ -247,31 +215,25 @@ article {
       }
     }
 
-    h3 {
-      font-size: 1.5rem;
-      color: rgb(1, 71, 36);
-      margin: 0px;
-      padding-bottom: 10px;
-    }
-
     .input-label {
       left: 20px;
+      margin-left: 10px;
       font-size: 1rem;
       color: rgb(0, 0, 0);
       pointer-events: none;
     }
-
     .input-slider {
-      margin: 0px;
+      margin-left: 10px;
       padding-right: 100 px;
       left: 20px;
       width: 100%;
       height: 80px;
     }
-
     .actions {
+      display: flex;
+      align-items: center;
+      justify-content: center;
       margin: 0;
-
       button {
         font-family: "noto Sans", sans-serif;
         outline: none;
@@ -290,11 +252,9 @@ article {
 .animate-in {
   animation: in 0.4s ease-in-out;
 }
-
 .animate-wrong {
   animation: wrong 0.6s ease-in-out;
 }
-
 @keyframes in {
   0% {
     opacity: 0;
@@ -305,7 +265,6 @@ article {
     transform: rotateY(0deg);
   }
 }
-
 @keyframes wrong {
   0% {
     transform: translateX(0);
